37229385cc2c12893ccc3c8fd0f0b061c0b569b9
michalwanto <59409764+michalwanto@users.noreply.github.com>
2020-09-25 21:10:20 +1000

    added font-display: swap;

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,7 @@ import React from "react";
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className="pa2">
-      <label></label>
+      <label> </label>
       <input
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
